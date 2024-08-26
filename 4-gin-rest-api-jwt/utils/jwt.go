ce0fa05992cc806c9ce284f82b2108109020b04e
Rajesh P. <rajp.gr8@gmail.com>
2024-08-26 14:50:24 +0530

    Update jwt.go

--- 4-gin-rest-api-jwt/utils/jwt.go
+++ 4-gin-rest-api-jwt/utils/jwt.go
@@ -19,3 +19,32 @@ func GenerateToken(email string, userId int64) (string, error) {
 }
 
 // See example to use jwt  https://github.com/snowflakedb/gosnowflake/blob/aa4b7cd4a56e74ac9871595a5e3625e9359ff3ab/auth.go#L482-L508
+/*
+func prepareJWTToken(config *Config) (string, error) {
+	pubBytes, err := x509.MarshalPKIXPublicKey(config.PrivateKey.Public())
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.Sum256(pubBytes)
+
+	accountName := extractAccountName(config.Account)
+	userName := strings.ToUpper(config.User)
+
+	issueAtTime := time.Now().UTC()
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"iss": fmt.Sprintf("%s.%s.%s", accountName, userName, "SHA256:"+base64.StdEncoding.EncodeToString(hash[:])),
+		"sub": fmt.Sprintf("%s.%s", accountName, userName),
+		"iat": issueAtTime.Unix(),
+		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		"exp": issueAtTime.Add(config.JWTExpireTimeout).Unix(),
+	})
+
+	tokenString, err := token.SignedString(config.PrivateKey)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, err
+}
+*/
