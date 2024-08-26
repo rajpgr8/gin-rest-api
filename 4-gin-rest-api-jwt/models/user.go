207bd7d2f19a555009eb91ae633cd63ac79afc85
rajp.gr8@gmail.com <rajp.gr8@gmail.com>
2024-08-26 14:10:29 +0530

    add jwt

--- 4-gin-rest-api-jwt/models/user.go
+++ 4-gin-rest-api-jwt/models/user.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"gin-rest-api/utils"
+)
+
 type User struct {
 	ID       int64
 	Email    string `binding:"required"`
@@ -8,11 +12,38 @@ type User struct {
 
 var users = []User{}
 
-func (e User) Save() {
+func (u User) Save() error {
 	// later: add it to a database
-	users = append(users, e)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = hashedPassword
+	users = append(users, u)
+
+	return err
 }
 
-func GetAllUsers() []User {
-	return users
+func (u User) ValidateCredentials() error {
+
+	// fetch password for the u.Email from the database
+	//query := "SELECT id, password FROM users WHERE email = ?"
+	//row := db.DB.QueryRow(query, u.Email)
+	//
+	//var retrievedPassword string
+	//err := row.Scan(&u.ID, &retrievedPassword)
+	//
+	//if err != nil {
+	//	return errors.New("Credentials invalid")
+	//}
+	//
+	//passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	//if !passwordIsValid {
+	//	return errors.New("Credentials invalid")
+	//}
+	//
+	return nil
 }
--- 4-gin-rest-api-jwt/readme.md
+++ 4-gin-rest-api-jwt/readme.md
@@ -5,4 +5,18 @@ TEST:
 Go to api-test folder.
 GET  get-event.http -> click on 'Send Request'
 POST create-event.http -> click on 'Send Request'
+
+POST login.http -> click on 'Send Request'
+Output:
+{
+  "message": "Login successful!",
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQGV4YW1wbGUuY29tIiwiZXhwIjoxNzI0NjY4MzI2LCJ1c2VySWQiOjB9.FoNXi6S8jrHPRM0_pQUmXtcyasAnOAxfDTGOVcDKc_E"
+}
+
+In authentication, when the user successfully logs in using their credentials, a JSON Web Token will be returned. 
+
+Whenever the user wants to access a protected route or resource, the user agent should send the JWT, typically in the Authorization header using the Bearer schema. The content of the header should look like the following:
+
+Authorization: Bearer <jwt-token>
+
 ```
