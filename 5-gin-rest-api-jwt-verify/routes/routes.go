c704511e6a795e94ad175669b6c4428fc22aeba5
Rajesh P. <rajp.gr8@gmail.com>
2024-09-02 13:36:46 +0100

    Update routes.go

--- 5-gin-rest-api-jwt-verify/routes/routes.go
+++ 5-gin-rest-api-jwt-verify/routes/routes.go
@@ -3,7 +3,7 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.GET("/events", getEvents) 
 	server.POST("/events", createEvent)
 	server.POST("/signup", signup)
 	server.POST("/login", login)
